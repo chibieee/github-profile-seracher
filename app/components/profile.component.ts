695ab694eb1a864814922aa2241c0221b78fc21e
chibieee <gregneo21@gmail.com>
2016-08-17 18:43:45 -0400

    "added username search bar"

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/map';
 export class ProfileComponent implements OnInit {
     user: any[];
     repos: any[];
+    username: string;
 
     constructor(private _githubService: GithubService) {
         this._githubService.getUser().subscribe(user => {
@@ -18,6 +19,18 @@ export class ProfileComponent implements OnInit {
 
         this._githubService.getRepos().subscribe(repos => {
             this.repos = repos;
+        })
+     }
+
+     searchUser() {
+        this._githubService.updateUser(this.username);
+
+        this._githubService.getUser().subscribe(user => {
+        this.user = user;
+        })
+
+        this._githubService.getRepos().subscribe(repos => {
+        this.repos = repos;
         })
      }
 
--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -8,19 +8,25 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService {
     private username: string;
+    private client_id = 'bbcb92d9ee42b4d9acf1';
+    private client_secret = '0aae42d973936729d7251344bc98acd918425648';
 
     constructor(private _http: Http) {
         this.username = 'chibieee';
      }
 
      getUser() {
-         return this._http.get('http://api.github.com/users/' + this.username)
+         return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(res => res.json())
      }
 
      getRepos() {
-         return this._http.get('http://api.github.com/users/' + this.username + '/repos')
+         return this._http.get('http://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(res => res.json())
+     }
+
+     updateUser(username:string) {
+         this.username = username;
      }
 
 }
